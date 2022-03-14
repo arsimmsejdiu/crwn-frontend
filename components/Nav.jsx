124ef10faa6ba81b086d6d2498e97b7b64abb12d
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-03-14 15:02:03 +0100

    Created the user Authentication query

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
-import NavStyle from './styles/NavStyles';
+import NavStyle from "./styles/NavStyles";
+import { useUser } from "./User";
 
 export default function Nav() {
+  const user = useUser();
+  console.log(user);
   return (
     <NavStyle>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Signin</Link>
+        </>
+      )}
     </NavStyle>
   );
 }
