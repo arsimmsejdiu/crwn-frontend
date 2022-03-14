b8ae90dca2f3ba1428fc48f10a64229977fae03d
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-03-14 15:38:48 +0100

    Added my cart to the nav bar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
-import NavStyle from "./styles/NavStyles";
+import { useCart } from "../lib/cartState";
+import CartCount from "./CartCount";
 import SignOut from "./SignOut";
+import NavStyle from "./styles/NavStyles";
 import { useUser } from "./User";
 
 export default function Nav() {
   const user = useUser();
-  console.log(user);
+  //const { openCart } = useCart();
   return (
     <NavStyle>
       <Link href="/products">Products</Link>
@@ -15,6 +17,11 @@ export default function Nav() {
           <Link href="/orders">Orders</Link>
           <Link href="/account">Account</Link>
           <SignOut />
+          {/* TODO: ad this to the button onClick={openCart} */}
+          <button type="button">
+            My Cart
+            <CartCount count={1}/>
+          </button>
         </>
       )}
       {!user && (
